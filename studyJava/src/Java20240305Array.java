8a4184e6b50a43ce30baf98abf854e8331d5530e
Jae-hoDo <132924223+Jae-hoDo@users.noreply.github.com>
2024-03-05 10:34:39 +0900

    change

--- studyJava/src/Java20240305Array.java
+++ studyJava/src/Java20240305Array.java
@@ -5,5 +5,15 @@ public class Java20240305Array {
         int intArray2[]; // C언어 스타일이고 쓰이지 않음
 
         // 배열 객체 생성
+        int[] intArray3 = new int[5];   // 메모리에 5개의 빈 저장소를 만듦
+        // 배열 객체에 값 입력
+        intArray3[0] = 10;
+        intArray3[1] = 20;
+        intArray3[2] = 30;
+        intArray3[3] = 40;
+        intArray3[4] = 50;
+        for(int i = 0 ; i < 5 ; i++) {
+            System.out.print(intArray3[i] + " ");
+        }
     }
 }
